8f0fe07a795e0675c3fbc3bb67b190ee7963e3c8
vchikunov <valchikunov@gmail.com>
2017-04-19 21:50:56 +0300

    Added base functionality

--- ospecanrest/controllers/api/star_catalog.py
+++ ospecanrest/controllers/api/star_catalog.py
@@ -1,39 +1,52 @@
 import pecan
 from pecan import rest, response
-from wsmeext.pecan import wsexpose
 from ospecanrest.model import star
 
 # Temporary solution. In the future will be add database and sqlalchemy
-stars = [star.Star("Aldebaran", 0), star.Star("Sun", 0)]
+stars = {1: star.Star("Aldebaran", 0),
+         2: star.Star("Sun", 0)}
 
 
 class StarCatalogController(rest.RestController):
+    key_index = 2
 
-    @wsexpose([star.Star])
-    def get(self):
-        return stars
-
-    @wsexpose(star.Star, int)
-    def get(self, objectId):
-        return star.Star("Aldebaran", 12)
+    @pecan.expose('json', content_type='application/json')
+    def get(self, object_id):
+        try:
+            response.status = 200
+            return stars[int(object_id)]
+        except (IndexError, ValueError, KeyError) as ex:
+            response.status = 404
+            return
 
     @pecan.expose()
     def post(self):
-        response.status = 200
-        return
-
-    @pecan.expose(bool, star.Star)
-    def put(self, new_star):
-        stars.append(new_star)
-        response.status = 200
-        return
-
-    @pecan.expose(bool, int)
-    def delete(self):
+        name = pecan.request.POST.get('name')
+        weight = pecan.request.POST.get('weight')
+        self.key_index += 1
+        stars[self.key_index] = star.Star(name, weight)
         response.status = 200
         return
 
     @pecan.expose()
-    def patch(self):
-        response.status = 201
-        return
+    def put(self, object_id):
+        try:
+            name = pecan.request.POST.get('name')
+            weight = pecan.request.POST.get('weight')
+            stars[int(object_id)] = star.Star(name, weight)
+            response.status = 200
+            return
+        except (IndexError, ValueError, KeyError) as ex:
+            response.status = 404
+            return
+
+    @pecan.expose()
+    def delete(self, object_id):
+        try:
+            response.status = 200
+            print(object_id)
+            stars.pop(int(object_id))
+            return
+        except KeyError as ex:
+            response.status = 404
+            return
